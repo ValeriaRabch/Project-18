c1d7cf0ab06f44a0bc96b8a6fe3e37f792790450
ValeriaRabch <rabchenukv@gmail.com>
2024-03-10 12:26:01 +0200

    Add func Count

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,17 @@
 #include<string.h>
 using namespace std;
 
-
+int Count(FILE* f) {
+	char s = getc(f);
+	int count = 0;
+	for (; s != EOF; s = getc(f)) {
+		if (s == '\n') {
+			count++;
+		}
+	}
+	fseek(f, 0, SEEK_SET);
+	return count;
+}
 
 int main() {
 	//завдання1
@@ -14,6 +24,11 @@ FILE* file1, * file2;
 file1 = fopen("D:\\Valeria\\file.txt", "r");
 file2 = fopen("D:\\Valeria\\file2.txt", "w");
 
+char line[100];
+int c;
+
+
+c = Count(file1);
 
 fclose(file1);
 fclose(file2); 
