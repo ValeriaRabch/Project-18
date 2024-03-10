533ad3f124f77b49844606e24bdb9985773ee64d
ValeriaRabch <rabchenukv@gmail.com>
2024-03-10 12:30:36 +0200

    Create file1, file2

--- Source.cpp
+++ Source.cpp
@@ -79,4 +79,12 @@ int main() {
 ////fclose(file1);
 ////fclose(file2); 
 
+
+	//завдання 3
+	FILE* file1, * file2;
+	file1 = fopen("D:\\Valeria\\file.txt", "r");
+	file2 = fopen("D:\\Valeria\\file2.txt", "w");
+
+	fclose(file1);
+	fclose(file2)
 }
